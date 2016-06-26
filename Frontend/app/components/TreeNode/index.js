d3bce9b6e43eeb80ea5049b5eaa3b5c48cb6293c
Flak <ali76034@gmail.com>
2016-06-25 23:10:40 -0400

    fixed classes

--- Frontend/app/components/TreeNode/index.js
+++ Frontend/app/components/TreeNode/index.js
@@ -31,20 +31,20 @@ var TreeNode = React.createClass({
     return (
       <div>
       <li style={styles} onClick={this.toggle}>
-        <div class='post clearfix reply'>
-          <div class='quote-level'>
-            <div class='message-container'>
-              <div class='person-container'>
-                <div class='name'>{this.props.node.name}</div>
+        <div className='post clearfix reply'>
+          <div className='quote-level'>
+            <div className='message-container'>
+              <div className='person-container'>
+                <div className='name'>{this.props.node.name}</div>
               </div>
-              <div class='date-container'>
-                <div class='time'>{this.props.node.time}</div>
-                <div class='date'>{this.props.node.date}</div>
+              <div className='date-container'>
+                <div className='time'>{this.props.node.time}</div>
+                <div className='date'>{this.props.node.date}</div>
               </div>
-              <div class='message'>{this.props.node.comment}</div>
+              <div className='message'>{this.props.node.comment}</div>
               <footer>
-                <div class='reply-button'><a href='#'><img src='/images/reply.gif' width="25" border='0' /></a></div>
-                <div class='load-replies-container'><a  href='#'>Load Replies</a></div>
+                <div className='reply-button'><a href='#'><img src='/images/reply.gif' width="25" border='0' /></a></div>
+                <div className='load-replies-container'><a  href='#'>Load Replies</a></div>
               </footer>
             </div>
           </div>
