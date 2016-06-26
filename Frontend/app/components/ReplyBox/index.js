b73f2e431cd5731efed51eb59ddf1b94af9e51ec
Flak <ali76034@gmail.com>
2016-06-26 14:28:07 -0400

    added static

--- Frontend/app/components/ReplyBox/index.js
+++ Frontend/app/components/ReplyBox/index.js
@@ -9,40 +9,10 @@ import {FormGroup, FormControl, HelpBlock, Button} from 'react-bootstrap'
 import styles from './styles.css';
 
 const ReplyBox = React.createClass({
-  getInitialState() {
-    return {
-      value: ''
-    };
-  },
-
-  getValidationState() {
-    const length = this.state.value.length;
-  },
-
-  handleChange(e) {
-    e.preventDefault();
-    this.setState({ value: e.target.value });
-  },
 
   render() {
     return (
-        <form>
-          <FormGroup
-              controlId="formBasicText"
-              validationState={this.getValidationState()}
-          >
-            <FormControl
-                type="text"
-                value={this.state.value}
-                placeholder="Enter text"
-                onChange={this.handleChange}
-            />
-            <HelpBlock>Validation is based on string length.</HelpBlock>
-          </FormGroup>
-          <Button type="submit">
-            Submit
-          </Button>
-        </form>
+        <div> <iframe src="107.170.125.104"></iframe> </div>
     );
   }
 });
